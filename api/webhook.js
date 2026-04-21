550494b3d24077f2cad2a2b8be051d6558a66955
codemonkey <50035424+codemonkey0612@users.noreply.github.com>
2026-04-22 00:05:25 +0900

    Update GAS_URL to new endpoint

--- api/webhook.js
+++ api/webhook.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
     console.log(req.body);
     // ★ バックグラウンドでGASに転送
     const GAS_URL =
-      'https://script.google.com/macros/s/AKfycbz_57FwvdVIX9g1teHsBy-6Ema3g1vqGmig-X2w2ujIjTznKaBJbhntYYcSsnLfc_hhfQ/exec';
+      'https://script.google.com/macros/s/AKfycbw6VZrxRWju9vyjwf-NDvyeXsdo9kQZSD_HYjWkjWPmQUFA0-NnClRNgv8E-EayfnZEbA/exec';
     try {
       await forwardToGas(GAS_URL, req.body);
       console.log('GASに転送完了');
